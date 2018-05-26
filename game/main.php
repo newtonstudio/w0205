a1379c606a70685e9a87a83a58cd0f5f58a18db5
newtonstudio <newtonstudio@gmail.com>
2018-05-26 12:19:40 +0800

    Add Bird

--- game/main.php
+++ game/main.php
@@ -9,12 +9,17 @@ $roger2 = new Rabbit("Roger Rabbit 2");
 $tortoise = new Tortoise("David Tortoise");
 $tortoise2 = new Tortoise("David Tortoise 2");
 
+class Bird extends Animal {
+
+}
+$bbb = new Bird("Tawk");
 
 $game = new Game();
 $game->partipant_join($roger);
 $game->partipant_join($roger2);
 $game->partipant_join($tortoise);
 $game->partipant_join($tortoise2);
+$game->partipant_join($bbb);
 
 $game->everyone_greet();
 
